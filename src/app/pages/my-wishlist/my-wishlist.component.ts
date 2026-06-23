a739cd2ce34053054726346145c82d1ec81ce68a
narriman <narimanamer805@gmail.com>
2026-06-23 17:15:31 +0300

    Create header component

---
+++ src/app/pages/my-wishlist/my-wishlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-wishlist',
+  imports: [],
+  templateUrl: './my-wishlist.component.html',
+  styleUrl: './my-wishlist.component.scss',
+})
+export default class MyWishlistComponent {
+
+}
